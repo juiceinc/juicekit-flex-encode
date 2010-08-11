9f498f41f689e6eb68a9f2cfb47e18a6b16c1cbe
CGemignani <cgemignani@chris-gemignanis-macbook-pro-3.local>
2010-08-11 11:06:23 -0400

    Fix names of package description in scripts/

--- scripts/docs.bash
+++ scripts/docs.bash
@@ -1,6 +1,6 @@
 # http://gauravj.com/blog/2009/06/setting-asdoc-description-for-packages/
-echo "Run from the main directory of juicekit-flex-data to generate asdocs"
+echo "Run from the main directory of juicekit-flex-encode to generate asdocs"
 echo ""
 echo "Output will be generated in target/asdoc-output/"
-asdoc -doc-sources+=src/main/flex -package-description-file scripts/package-description-core.xml -main-title "JuiceKit Data API Documentation" -output target/asdoc-output/
+asdoc -doc-sources+=src/main/flex -external-library-path+=../juicekit-flex-core/bin/juicekit-flex-core.swc -external-library-path+=../juicekit-flex-data/bin/juicekit-flex-data.swc -package-description-file scripts/package-description-encode.xml -main-title "JuiceKit Encode API Documentation" -output target/asdoc-output/
 open target/asdoc-output/index.html
